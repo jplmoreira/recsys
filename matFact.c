dd170a57cd3850348d16d2e85b6c730734b5ed2f
jplmoreira <jplmoreira92@gmail.com>
2020-03-24 19:17:01 +0000

    Fix matrix representation

--- matFact.c
+++ matFact.c
@@ -2,169 +2,128 @@
 #include <stdlib.h>
 #include <string.h>
 
-
 #define RAND01 ((double)random() / (double)RAND_MAX)
 
-double*** L;
-double*** R;
-double** B;
-double** A;
-int cur = 0, prev = 1;
-double* A_val;
-int* A_l;
-int* A_c;
-
-void random_fill_LR(int nU, int nI, int nF)
-{
-  /* srandom(0); */
-  /* for(int i = 0; i < nU; i++) */
-  /*   for(int j = 0; j < nF; j++) */
-  /*     L[cur][i][j] = RAND01 / (double) nF; */
-  /* for(int i = 0; i < nF; i++) */
-  /*   for(int j = 0; j < nI; j++) */
-  /*     R[cur][i][j] = RAND01 / (double) nF; */
-  memcpy(L[cur][0], (double[]){ 0.420094, 0.197191 }, sizeof(double)*2);
-  memcpy(L[cur][1], (double[]){ 0.391550, 0.399220 }, sizeof(double)*2);
-  memcpy(L[cur][2], (double[]){ 0.455824, 0.098776 }, sizeof(double)*2);
-  memcpy(R[cur][0], (double[]){ 0.167611, 0.384115, 0.138887, 0.276985, 0.238699 }, sizeof(double)*5);
-  memcpy(R[cur][1], (double[]){ 0.314435, 0.182392, 0.256700, 0.476115, 0.458098 }, sizeof(double)*5);
+void random_fill_LR(int nU, int nI, int nF, double *L, double *R) {
+  srandom(1);
+  for(int i = 0; i < nU; i++)
+    for(int j = 0; j < nF; j++)
+      L[i*nF + j] = RAND01 / (double) nF;
+  for(int i = 0; i < nF; i++)
+    for(int j = 0; j < nI; j++)
+      R[i*nI + j] = RAND01 / (double) nF;
 }
 
 
-void calculate_B(int num_rows, int num_colums, int num_feats){
-  int i, j, k;
-  double sum = 0;	
-  for(i = 0; i < num_rows; i++){
-    for(j = 0; j < num_colums; j++){
-      for(k = 0; k < num_feats; k++){
-        sum = sum + L[cur][i][k]*R[cur][k][j];
+void calculate_B(int num_rows, int num_columns, int num_feats, double *B, double *L, double *R){
+  for(int i = 0; i < num_rows; i++){
+    for(int j = 0; j < num_columns; j++){
+      double sum = 0;	
+      for(int k = 0; k < num_feats; k++){
+        sum += L[i*num_feats + k]*R[k*num_columns + j];
       }
-      B[i][j] = sum;
-      sum = 0;
+      B[i*num_columns + j] = sum;
     }	
   }
 }
 
-void calculate_L_and_R(int num_rows, int num_colums, int num_feats, double alpha){
-  int l, c;
-  int k, i;
-  double delta;
-  prev = cur;
-  cur = ++cur % 2;
-  for(l = 0; l < num_rows; l++){
-    for(c = 0; c < num_colums; c++){
-      if(A[l][c] != 0){
-        delta = A[l][c] - B[l][c];
-        for(k = 0; k < num_feats; k++){
-          L[cur][l][k] = L[cur][l][k] - alpha*2*delta*(-R[prev][k][c]);
-          R[cur][k][c] = R[cur][k][c] - alpha*2*delta*(-L[prev][l][k]);
-        }
-      } 
+void calculate_L_and_R(int num_rows, int num_columns, int num_feats, int num_non_zeros,
+                       double alpha, double *A_val, int *A_r, int *A_c,
+                       double *B, double *L, double *R){
+
+  double *L_copy = (double *)malloc(sizeof(double) * num_rows * num_feats);
+  memcpy(L_copy, L, sizeof(double) * num_rows * num_feats);
+  double *R_copy = (double *)malloc(sizeof(double) * num_feats * num_columns);
+  memcpy(R_copy, R, sizeof(double) * num_feats * num_columns);
+
+  for (int n = 0; n < num_non_zeros; n++) {
+    double delta = A_val[n] - B[A_r[n]*num_columns + A_c[n]];
+    for (int f = 0; f < num_feats; f++) {
+      L[A_r[n]*num_feats + f] += alpha*2*delta*R_copy[f*num_columns + A_c[n]];
+      R[f*num_columns + A_c[n]] += alpha*2*delta*L_copy[A_r[n]*num_feats + f];
     }
   }
+
+  free(L_copy);
+  free(R_copy);
 }
     
-
+int is_non_zero(int row, int col, int num_non_zeros, int *A_r, int *A_c) {
+  for (int i = 0; i < num_non_zeros; i++) {
+    if (row == A_r[i] && col == A_c[i])
+      return 1;
+  }
+  return 0;
+}
 
 int main(int argc, char **argv){
+  if (argc != 2) {
+    printf("Wrong usage: matFact [input file]\n");
+    return 1;
+  }
+  
   FILE *f = fopen(argv[1], "r");
-  int num_iters;
-  double alpha;
-  int num_feats;
-  int num_rows, num_colums, num_non_zeros;
-  int row, col;
-  double value;
+  int num_iters, num_feats, num_rows, num_columns, num_non_zeros;
+  double alpha; 
+
   fscanf (f, "%d", &num_iters);
   fscanf (f, "%lf", &alpha);
   fscanf (f, "%d", &num_feats);
-  fscanf (f, "%d %d %d", &num_rows, &num_colums, &num_non_zeros);
+  fscanf (f, "%d %d %d", &num_rows, &num_columns, &num_non_zeros);
     
+  double* L = (double *)malloc(sizeof(double) * num_rows * num_feats);
+    
+  double *R = (double *)malloc(sizeof(double) * num_feats * num_columns);
+
+  double *B = (double *)malloc(sizeof(double) * num_rows * num_columns);
   
-  L = (double ***)malloc(2 * sizeof(double**));
-  L[0] = (double **)malloc(num_rows * sizeof(double*));
-  L[1] = (double **)malloc(num_rows * sizeof(double*));
-  for(int d = 0; d < num_rows; d++) {	
-    L[0][d] = (double *)malloc(num_feats * sizeof(double));
-    L[1][d] = (double *)malloc(num_feats * sizeof(double));
-  }
+  double *A_val = (double *)malloc(sizeof(double) * num_non_zeros);
+  int *A_r = (int *)malloc(sizeof(int) * num_non_zeros);
+  int *A_c = (int *)malloc(sizeof(int) * num_non_zeros);
     
-  R = (double ***)malloc(2 * sizeof(double**));
-  R[0] = (double **)malloc(num_feats * sizeof(double*));
-  R[1] = (double **)malloc(num_feats * sizeof(double*));
-  for(int d = 0; d < num_feats; d++) {
-    R[0][d] = (double *)malloc(num_colums * sizeof(double));
-    R[1][d] = (double *)malloc(num_colums * sizeof(double));
-  }
-    
-  B = (double **)malloc(num_rows * sizeof(double));
-  for(int d = 0; d < num_rows; d++){
-    B[d] = (double *)malloc(num_colums * sizeof(double));
-  }
-    
-  A = (double **)malloc(num_rows * sizeof(double));
-  for(int d = 0; d < num_rows; d++){
-    A[d] = (double *)malloc(num_colums * sizeof(double));
-  }
-    
-  //double A[num_rows][num_colums];
-  for(int i = 0; i < num_rows; i++){
-    for(int j = 0; j < num_colums; j++){
-      A[i][j] = 0;
-    }
-  }
-  while(fscanf(f, "%d %d %lf", &row, &col, &value) != EOF){
+  for (int i = 0; i < num_non_zeros; i++) {
+    int row, col;
+    double value;
+    fscanf(f, "%d %d %lf", &row, &col, &value);
     if (value < 1 || value > 5){
-      printf("Rating is not between 1 and 5");
+      printf("Rating is not between 1 and 5\n");
       return 1;
     }
-    A[row][col] = value;
-    //printf("----->%f\n", A[row][col]);
+    A_r[i] = row;
+    A_c[i] = col;
+    A_val[i] = value;
   }
   fclose(f);
-  random_fill_LR(num_rows, num_colums, num_feats);
-  calculate_B(num_rows, num_colums, num_feats);
-  int i = 0;
-  while(i < num_iters){
-    calculate_L_and_R(num_rows, num_colums, num_feats, alpha);
-    calculate_B(num_rows, num_colums, num_feats);
-    i++;
+
+  random_fill_LR(num_rows, num_columns, num_feats, L, R);
+  calculate_B(num_rows, num_columns, num_feats, B, L, R);
+
+  for (int i = 0; i < num_iters; i++) {
+    calculate_L_and_R(num_rows, num_columns, num_feats, num_non_zeros,
+                      alpha, A_val, A_r, A_c, B, L, R);
+    calculate_B(num_rows, num_columns, num_feats, B, L, R);
   }
+
   for(int i = 0; i < num_rows; i++){
     double max = 0;
     int index;
-    for(int j = 0; j < num_colums; j++){
-      if(A[i][j] == 0){
-        if(B[i][j] > max){
-          /* printf("----->%f\n", max); */
-          max = B[i][j];
+    for(int j = 0; j < num_columns; j++){
+      if(!is_non_zero(i, j, num_non_zeros, A_r, A_c)){
+        if(B[i*num_columns + j] > max){
+          max = B[i*num_columns + j];
           index = j;
         }
       }
     }
-    max = 0;
     printf("%d\n", index);
   }
-  /*int flot;
-	fscanf (f, "%d", &flot);
-	fclose(f);
-	printf("----> %d", flot);*/
-  /* printf("\nFinal Matrix L\n"); */
-  /* for (int i = 0; i < num_rows; i++) { */
-  /*   for (int j = 0; j < num_feats; j++) */
-  /*     printf("%lf ", L[cur][i][j]); */
-  /*   printf("\n"); */
-  /* } */
-  /* printf("\nFinal Matrix R\n"); */
-  /* for (int i = 0; i < num_feats; i++) { */
-  /*   for (int j = 0; j < num_colums; j++) */
-  /*     printf("%lf ", R[cur][i][j]); */
-  /*   printf("\n"); */
-  /* } */
-  /* printf("\nFinal Matrix B\n"); */
-  /* for (int l = 0; l < num_rows; l++) { */
-  /*   for (int c = 0; c < num_colums; c++) */
-  /*     printf("%lf ", B[l][c]); */
-  /*   printf("\n"); */
-  /* } */
-  return 1;
+
+  free(L);
+  free(R);
+  free(B);
+  free(A_val);
+  free(A_r);
+  free(A_c);
+
+  return 0;
 }	
